f85535f05ad0d70877223625db68e76351f84d8f
AllenLVieira <allenvieira96@gmail.com>
2021-11-18 20:47:28 -0300

    First project commit

---
+++ src/main/java/br/com/allen/flashfood/FlashfoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.allen.flashfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashfoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/allen/flashfood/FlashfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.allen.flashfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
