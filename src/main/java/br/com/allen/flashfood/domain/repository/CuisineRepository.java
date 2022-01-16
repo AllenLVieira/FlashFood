b9ba232b8ccd2c10b670fd1292ea7f3ea8de77c6
AllenLVieira <allenvieira96@gmail.com>
2022-01-16 18:38:16 -0300

    [feat]: Implement queries Cuisine and Restaurant

--- src/main/java/br/com/allen/flashfood/domain/repository/CuisineRepository.java
+++ src/main/java/br/com/allen/flashfood/domain/repository/CuisineRepository.java
@@ -4,6 +4,9 @@ import br.com.allen.flashfood.domain.model.Cuisine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CuisineRepository extends JpaRepository<Cuisine, Long> {
+    List<Cuisine> findByNameContainingIgnoreCase(String name);
 }
--- src/main/java/br/com/allen/flashfood/domain/repository/RestaurantRepository.java
+++ src/main/java/br/com/allen/flashfood/domain/repository/RestaurantRepository.java
@@ -2,8 +2,19 @@ package br.com.allen.flashfood.domain.repository;
 
 import br.com.allen.flashfood.domain.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    @Query("from Restaurant where name like %:name% and cuisine.id = :id")
+    List<Restaurant> findByNameAndCuisineId(String name, @Param("id") Long cuisineId);
+
+    List<Restaurant> findByFreightRateBetween(BigDecimal initalFee, BigDecimal finalFee);
+
+    int countByCuisineId(Long cuisineId);
 }
