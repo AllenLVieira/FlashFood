6a2903b2e3f4450ed769db93e2a1a806c6a67ba9
AllenLVieira <allenvieira96@gmail.com>
2022-01-25 23:38:07 -0300

    [style]: Improve readability of import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,21 +1,15 @@
-insert into state (id, name) values (1, 'São Paulo');
-insert into state (id, name) values (2, 'Bahia');
-insert into state (id, name) values (3, 'Pernambuco');
+INSERT INTO state (id, name) VALUES (1, 'São Paulo'),(2, 'Bahia'),(3, 'Pernambuco');
 
-insert into city (id, name, state_id) values (1, 'São Paulo', 1);
-insert into city (id, name, state_id) values (2, 'Salvador', 2);
-insert into city (id, name, state_id) values (3, 'Recife', 3);
+INSERT INTO city (id, name, state_id) VALUES (1, 'São Paulo', 1),(2, 'Salvador', 2),(3, 'Recife', 3);
 
-insert into cuisine (id, name) values(1, 'Italian');
-insert into cuisine (id, name) values(2, 'Brazilian');
+INSERT INTO cuisine (id, name) VALUES (1, 'Italian'),(2, 'Brazilian');
 
-INSERT INTO restaurant (name,freight_rate,cuisine_id,address_zipcode,address_street,address_number,address_city_id,address_district,address_complement) VALUES ('Pecorino',16.9,1,'04285-001','Rua Elba','909',1,'Vila Moinho Velho','Casa 2');
-INSERT INTO restaurant (name,freight_rate,cuisine_id,address_zipcode,address_street,address_number,address_city_id,address_district,address_complement) VALUES ('Soho',30,2,'40015-160','Rua Lafayete Coutinho','1010',2,'Bahia Marina','');
+INSERT INTO restaurant (name, freight_rate, cuisine_id, registration_date,update_date, address_zipcode, address_street, address_number, address_city_id, address_district, address_complement) VALUES('Pecorino', 16.9, 1, utc_timestamp, utc_timestamp, '04285-001', 'Rua Elba','909', 1, 'Vila Moinho Velho', 'Casa 2');
+INSERT INTO restaurant (name, freight_rate, cuisine_id, registration_date,update_date, address_zipcode, address_street, address_number, address_city_id, address_district, address_complement) VALUES('Soho', 30, 2, utc_timestamp, utc_timestamp,'40015-160', 'Rua Lafayete Coutinho','1010', 2, 'Bahia Marina', '');
 
-insert into payment_method (id, description) values (1, 'Credit Card');
-insert into payment_method (id, description) values (2, 'Debit Card');
-insert into payment_method (id, description) values (3, 'Money');
+INSERT INTO payment_method (id, description) VALUES (1, 'Credit Card'),(2, 'Debit Card'),(3, 'Money');
 
-insert into permission (id, name, description) values (1, 'CONSULT', 'Allows you consult cuisines');
-insert into permission (id, name, description) values (2, 'EDIT', 'Allows you to edit cuisines');
-insert into restaurant_payment_method (restaurant_id, payment_method_id) values (1,1), (1,2), (1,3), (2,3);
+INSERT INTO permission (id, name, description) VALUES (1, 'CONSULT', 'Allows you consult cuisines');
+INSERT INTO permission (id, name, description) VALUES (2, 'EDIT', 'Allows you to edit cuisines');
+
+INSERT INTO restaurant_payment_method (restaurant_id, payment_method_id) VALUES (1, 1),(1, 2),(1, 3),(2, 3);
