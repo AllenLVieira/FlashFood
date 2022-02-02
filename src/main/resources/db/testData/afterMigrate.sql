9cb632561caa51a58c70e64401a774a72d73e3af
AllenLVieira <allenvieira96@gmail.com>
2022-02-02 00:45:55 -0300

    [feat]: Flyway: test data

--- src/main/resources/db/testData/afterMigrate.sql
+++ src/main/resources/db/testData/afterMigrate.sql
@@ -1,3 +1,30 @@
+set foreign_key_checks = 0;
+
+delete from city;
+delete from cuisine;
+delete from family_permission;
+delete from family;
+delete from restaurant_payment_method;
+delete from restaurant;
+delete from product;
+delete from permission;
+delete from payment_method;
+delete from state;
+delete from user;
+delete from user_group;
+
+set foreign_key_checks = 1;
+
+alter table city auto_increment = 1;
+alter table cuisine auto_increment = 1;
+alter table family auto_increment = 1;
+alter table restaurant auto_increment = 1;
+alter table product auto_increment = 1;
+alter table permission auto_increment = 1;
+alter table payment_method auto_increment = 1;
+alter table state auto_increment = 1;
+alter table user auto_increment = 1;
+
 INSERT INTO state (id, name) VALUES (1, 'São Paulo'),(2, 'Bahia'),(3, 'Pernambuco');
 
 INSERT INTO city (id, name, state_id) VALUES (1, 'São Paulo', 1),(2, 'Salvador', 2),(3, 'Recife', 3);
