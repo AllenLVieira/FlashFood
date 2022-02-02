210d29bf884879178ff32759d6571cbf711a24e4
AllenLVieira <allenvieira96@gmail.com>
2022-02-01 21:19:25 -0300

    [feat]: Improve query performance N+1

--- src/main/java/br/com/allen/flashfood/domain/repository/RestaurantRepository.java
+++ src/main/java/br/com/allen/flashfood/domain/repository/RestaurantRepository.java
@@ -3,6 +3,7 @@ package br.com.allen.flashfood.domain.repository;
 import br.com.allen.flashfood.domain.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +14,14 @@ import java.util.List;
 public interface RestaurantRepository
         extends JpaRepository<Restaurant, Long>, RestaurantRepositoryQueries,
         JpaSpecificationExecutor<Restaurant> {
+
+    /**
+     * To improve query performance with fetch
+     * If you need to do the same with ManyToMany, can do a LEFT JOIN FETCH
+     */
+    @Query("from Restaurant r join fetch r.cuisine")
+    List<Restaurant> findAll();
+
     List<Restaurant> consultByNameAndBetweenFee(String name, BigDecimal initialFee, BigDecimal finalFee);
 
     List<Restaurant> consultByNameAndCuisineId(String name, @Param("id") Long cuisineId);
