7dfb28e76b3457926e8b52dc64186a41fb85e188
AllenLVieira <allenvieira96@gmail.com>
2022-04-18 22:44:07 -0300

    [fix]: Refining the registration payload with @JsonIgnoreProperties
    - Keep the payload appearing when searching for the restaurant;
    - Prevent the user from updating the cuisine name from the restaurant

--- src/main/java/br/com/allen/flashfood/domain/model/Restaurant.java
+++ src/main/java/br/com/allen/flashfood/domain/model/Restaurant.java
@@ -3,6 +3,7 @@ package br.com.allen.flashfood.domain.model;
 import br.com.allen.flashfood.core.validation.FreightRate;
 import br.com.allen.flashfood.core.validation.validationgroups.Groups;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.CreationTimestamp;
@@ -51,6 +52,7 @@ public class Restaurant {
     @JsonIgnore
     private LocalDateTime updateDate;
 
+    @JsonIgnoreProperties(value = "name", allowGetters = true)
     @ConvertGroup(from = Default.class, to = Groups.CuisineId.class)
     @Valid
     @NotNull
