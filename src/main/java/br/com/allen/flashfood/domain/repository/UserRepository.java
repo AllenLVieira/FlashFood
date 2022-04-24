c2ff84d3d7f194b879054b89df9c5cd2ced3cd71
AllenLVieira <allenvieira96@gmail.com>
2022-04-24 18:53:31 -0300

    [feat](user): Add user repository

---
+++ src/main/java/br/com/allen/flashfood/domain/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package br.com.allen.flashfood.domain.repository;
+
+import br.com.allen.flashfood.domain.model.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CustomJpaRepository<User, Long> {
+}
