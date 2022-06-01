2b749b8a16204f9ff4de83d9d0cd8dc347e63064
AllenLVieira <allenvieira96@gmail.com>
2022-05-31 21:50:46 -0300

    feat(managers): Add managers feature - Add controller; - Fix assembler; - Add methods to Service.

--- src/main/java/br/com/allen/flashfood/api/assembler/UserModelAssembler.java
+++ src/main/java/br/com/allen/flashfood/api/assembler/UserModelAssembler.java
@@ -6,6 +6,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,7 +19,7 @@ public class UserModelAssembler {
         return modelMapper.map(user, UserResponse.class);
     }
 
-    public List<UserResponse> toCollectionModel(List<User> userList) {
+    public List<UserResponse> toCollectionModel(Collection<User> userList) {
         return userList.stream()
                 .map(user -> toModel(user))
                 .collect(Collectors.toList());
