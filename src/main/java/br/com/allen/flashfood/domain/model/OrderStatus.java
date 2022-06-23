55957ccce609f86b01000846e5c5695a2c6e2941
AllenLVieira <allenvieira96@gmail.com>
2022-06-22 23:31:38 -0300

    refactor: Improve flexibility of status change code

--- src/main/java/br/com/allen/flashfood/domain/model/OrderStatus.java
+++ src/main/java/br/com/allen/flashfood/domain/model/OrderStatus.java
@@ -1,18 +1,28 @@
 package br.com.allen.flashfood.domain.model;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum OrderStatus {
     CREATED("Created"),
-    CONFIRMED("Confirmed"),
-    DELIVERED("Delivered"),
-    CANCELED("Canceled");
+    CONFIRMED("Confirmed", CREATED),
+    DELIVERED("Delivered", CONFIRMED),
+    CANCELED("Canceled", CREATED);
 
     private final String description;
 
-    OrderStatus(String description) {
+    private List<OrderStatus> previousStatus;
+
+    OrderStatus(String description, OrderStatus... previousStatus) {
         this.description = description;
+        this.previousStatus = Arrays.asList(previousStatus);
     }
 
     public String getDescription() {
         return description;
+    }
+
+    public boolean cannotChangeStatusTo(OrderStatus newStatus) {
+        return !newStatus.previousStatus.contains(this);
     }
 }
--- src/main/java/br/com/allen/flashfood/domain/service/OrderFlowService.java
+++ src/main/java/br/com/allen/flashfood/domain/service/OrderFlowService.java
@@ -1,13 +1,10 @@
 package br.com.allen.flashfood.domain.service;
 
-import br.com.allen.flashfood.domain.exception.BusinessException;
 import br.com.allen.flashfood.domain.model.DeliveryOrder;
-import br.com.allen.flashfood.domain.model.OrderStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.time.OffsetDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -18,44 +15,19 @@ public class OrderFlowService {
     @Transactional
     public void confirmOrder(Long orderId) {
         DeliveryOrder order = orderService.findOrderOrElseThrow(orderId);
-
-        validateOrderStatus(order, OrderStatus.CREATED, OrderStatus.CONFIRMED);
-
-        order.setStatus(OrderStatus.CONFIRMED);
-        order.setConfirmationDate(OffsetDateTime.now());
+        order.confirm();
     }
 
     @Transactional
     public void deliverOrder(Long orderId) {
         DeliveryOrder order = orderService.findOrderOrElseThrow(orderId);
-
-        validateOrderStatus(order, OrderStatus.CONFIRMED, OrderStatus.DELIVERED);
-
-        order.setStatus(OrderStatus.DELIVERED);
-        order.setDeliveryDate(OffsetDateTime.now());
+        order.deliver();
     }
 
     @Transactional
     public void cancelOrder(Long orderId) {
         DeliveryOrder order = orderService.findOrderOrElseThrow(orderId);
-
-        validateOrderStatus(order, OrderStatus.CREATED, OrderStatus.CANCELED);
-
-        order.setStatus(OrderStatus.CANCELED);
-        order.setCancellationDate(OffsetDateTime.now());
-    }
-
-    /**
-     * @param order        Order to be validate
-     * @param sourceStatus Initial status required for validation
-     * @param targetStatus Desired status on conversion
-     */
-    private void validateOrderStatus(DeliveryOrder order, OrderStatus sourceStatus, OrderStatus targetStatus) {
-        if (!order.getStatus().equals(sourceStatus)) {
-            throw new BusinessException(
-                    String.format("Order status %d cannot be changed from %s to %s.", order.getId(),
-                            order.getStatus().getDescription(), targetStatus.getDescription()));
-        }
+        order.cancel();
     }
 
 }
