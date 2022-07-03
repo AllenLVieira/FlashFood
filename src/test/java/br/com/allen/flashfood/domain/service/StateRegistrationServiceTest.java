115fab84f148bfee3bcc110fb5ccd4d4f382c56e
AllenLVieira <allenvieira96@gmail.com>
2022-07-03 01:05:36 -0300

    test: Add State Service tests

---
+++ src/test/java/br/com/allen/flashfood/domain/service/StateRegistrationServiceTest.java
@@ -0,0 +1,129 @@
+package br.com.allen.flashfood.domain.service;
+
+import br.com.allen.flashfood.domain.exception.EntityInUseException;
+import br.com.allen.flashfood.domain.exception.StateNotFoundException;
+import br.com.allen.flashfood.domain.model.State;
+import br.com.allen.flashfood.domain.repository.StateRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ContextConfiguration(classes = {StateRegistrationService.class})
+@ExtendWith(SpringExtension.class)
+class StateRegistrationServiceTest {
+    @Autowired
+    private StateRegistrationService underTest;
+
+    @MockBean
+    private StateRepository stateRepository;
+
+    State state;
+
+    @BeforeEach
+    void setUp() {
+        state = new State();
+        state.setId(123L);
+        state.setName("Name");
+    }
+
+    /**
+     * Method under test: {@link StateRegistrationService#saveState(State)}
+     */
+    @Test
+    void shouldSaveState() {
+        when(stateRepository.save(any())).thenReturn(state);
+
+        State state1 = new State();
+        state1.setId(123L);
+        state1.setName("Name");
+        assertSame(state, underTest.saveState(state1));
+        verify(stateRepository).save(any());
+    }
+
+    /**
+     * Method under test: {@link StateRegistrationService#saveState(State)}
+     */
+    @Test
+    void shouldThrowEmptyResultDataAccessExceptionWhenTryToSaveState() {
+        when(stateRepository.save(any())).thenThrow(new EmptyResultDataAccessException(3));
+
+        assertThrows(EmptyResultDataAccessException.class, () -> underTest.saveState(state));
+        verify(stateRepository).save(any());
+    }
+
+    /**
+     * Method under test: {@link StateRegistrationService#deleteState(Long)}
+     */
+    @Test
+    void shouldDeleteState() {
+        doNothing().when(stateRepository).flush();
+        doNothing().when(stateRepository).deleteById(any());
+        underTest.deleteState(123L);
+        verify(stateRepository).flush();
+        verify(stateRepository).deleteById(any());
+    }
+
+    /**
+     * Method under test: {@link StateRegistrationService#deleteState(Long)}
+     */
+    @Test
+    void shouldThrowsStateNotFoundExceptionWhenDeleteState() {
+        doThrow(new EmptyResultDataAccessException(3)).when(stateRepository).deleteById(any());
+        assertThrows(StateNotFoundException.class, () -> underTest.deleteState(123L));
+        verify(stateRepository).deleteById(any());
+    }
+
+    /**
+     * Method under test: {@link StateRegistrationService#deleteState(Long)}
+     */
+    @Test
+    void shouldThrowsEntityInUseExceptionWhenDeleteState() {
+        doThrow(new DataIntegrityViolationException("An error occurred")).when(stateRepository).deleteById(any());
+        assertThrows(EntityInUseException.class, () -> underTest.deleteState(123L));
+        verify(stateRepository).deleteById(any());
+    }
+
+    /**
+     * Method under test: {@link StateRegistrationService#findStateOrElseThrow(Long)}
+     */
+    @Test
+    void shouldFindStateById() {
+        Optional<State> ofResult = Optional.of(state);
+        when(stateRepository.findById(any())).thenReturn(ofResult);
+        assertSame(state, underTest.findStateOrElseThrow(123L));
+        verify(stateRepository).findById(any());
+    }
+
+    /**
+     * Method under test: {@link StateRegistrationService#findStateOrElseThrow(Long)}
+     */
+    @Test
+    void shouldThrowsStateNotFoundExceptionWhenTryToFindState() {
+        when(stateRepository.findById(any())).thenReturn(Optional.empty());
+        assertThrows(StateNotFoundException.class, () -> underTest.findStateOrElseThrow(123L));
+        verify(stateRepository).findById(any());
+    }
+
+    /**
+     * Method under test: {@link StateRegistrationService#findStateOrElseThrow(Long)}
+     */
+    @Test
+    void shouldThrowsEmptyResultDataAccessExceptionWhenTryToFindState() {
+        when(stateRepository.findById(any())).thenThrow(new EmptyResultDataAccessException(3));
+        assertThrows(EmptyResultDataAccessException.class, () -> underTest.findStateOrElseThrow(123L));
+        verify(stateRepository).findById(any());
+    }
+}
+
