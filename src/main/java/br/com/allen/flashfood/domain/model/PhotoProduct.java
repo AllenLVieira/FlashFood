37d22723a024bd3f4664d53ef346e1eb507a2f54
AllenLVieira <allenvieira96@gmail.com>
2023-12-29 20:12:58 -0300

    feat(product-photos): add PhotoProduct entity, table, and Flyway migration
    Model: Introduces the PhotoProduct entity to model product photos within the database.
     - Fields: filename, description, content type, filesize.
     - Relationship: One-to-one with Product using product ID as primary key.
     - Lombok annotations for data generation and equals/hashCode.
    Database: Creates the photo_product table via a Flyway migration.
    Migration: Extends afterMigrate.sql with necessary statements for photo handling.

---
+++ src/main/java/br/com/allen/flashfood/domain/model/PhotoProduct.java
@@ -0,0 +1,24 @@
+package br.com.allen.flashfood.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity(name = "photo_product")
+public class PhotoProduct {
+  @EqualsAndHashCode.Include
+  @Id
+  @Column(name = "product_id")
+  private long id;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @MapsId
+  private Product product;
+
+  private String filename;
+  private String description;
+  private String contentType;
+  private Long filesize;
+}
